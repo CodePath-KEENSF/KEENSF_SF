2faa5abcb76d4f551c25555c9ac07aa3bc0aa266
comscience15 <comscience15@hotmail.com>
2014-10-18 14:53:15 -0700

    Updated Coach check-in to be dropdown

--- src/org/keenusa/connect/adapters/CoachesCheckInAdapter.java
+++ src/org/keenusa/connect/adapters/CoachesCheckInAdapter.java
@@ -11,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,11 +24,13 @@ import android.widget.Toast;
 public class CoachesCheckInAdapter extends ArrayAdapter<CoachAttendance> {
 
 		TextView tvCoachCheckIn;
-		Button btnChkIn;
-		Button btnAbsent;
-		Button btnOthers;
+//		Button btnChkIn;
+//		Button btnAbsent;
+//		Button btnOthers;
 		Button btnAddCoach;
 		EditText etCoachSearch;
+		Spinner spinner;
+		String[] options = {"Check-In", "Absent", "Others"};
 
 	public CoachesCheckInAdapter(Context context, ArrayList<CoachAttendance> coachList) {
 		super(context, android.R.layout.simple_list_item_1, coachList);
@@ -41,37 +45,55 @@ public class CoachesCheckInAdapter extends ArrayAdapter<CoachAttendance> {
 			convertView = LayoutInflater.from(getContext()).inflate(R.layout.coach_check_in_items, parent, false);
 		}
 		tvCoachCheckIn = (TextView) convertView.findViewById(R.id.tvAthleteCheckIn);
-		btnChkIn = (Button) convertView.findViewById(R.id.btnChkIn);
-		btnAbsent = (Button) convertView.findViewById(R.id.btnAbsent);
-		btnOthers = (Button) convertView.findViewById(R.id.btnOthers);
+//		btnChkIn = (Button) convertView.findViewById(R.id.btnChkIn);
+//		btnAbsent = (Button) convertView.findViewById(R.id.btnAbsent);
+//		btnOthers = (Button) convertView.findViewById(R.id.btnOthers);
 		btnAddCoach = (Button) convertView.findViewById(R.id.btnAddCoach);
 		etCoachSearch = (EditText) convertView.findViewById(R.id.etCoachSearch);
 		ImageView ivCoachImageProfile = (ImageView) convertView.findViewById(R.id.ivCoachImageProfile);
+		spinner = (Spinner) convertView.findViewById(R.id.spCheckInOptions);
 		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, options);
+		spinner.setAdapter(adapter);
 		
-		tvCoachCheckIn.setText(coach.getAttendedCoachFullName().toString());
+ 		tvCoachCheckIn.setText(coach.getAttendedCoachFullName().toString());
+ 		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,int position, long id) {
+				int pos = spinner.getSelectedItemPosition();
+//				Toast.makeText(getContext(), options[+pos] + " is selected", Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				// TODO Auto-generated method stub
+				
+			}
+ 			
+		});
 //		ivCoachImageProfile.setImageResource(position);
-		btnChkIn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getContext(), "Check-in " + coach.getAttendedCoachFullName().toString(), Toast.LENGTH_SHORT).show();
-			}
-		});;
-		btnAbsent.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getContext(), "Absent " + coach.getAttendedCoachFullName().toString(), Toast.LENGTH_SHORT).show();
-			}
-		});;
-		btnOthers.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getContext(), "Others " + coach.getComments().toString(), Toast.LENGTH_SHORT).show();
-			}
-		});;
+//		btnChkIn.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Toast.makeText(getContext(), "Check-in " + coach.getAttendedCoachFullName().toString(), Toast.LENGTH_SHORT).show();
+//			}
+//		});;
+//		btnAbsent.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Toast.makeText(getContext(), "Absent " + coach.getAttendedCoachFullName().toString(), Toast.LENGTH_SHORT).show();
+//			}
+//		});;
+//		btnOthers.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Toast.makeText(getContext(), "Others " + coach.getComments().toString(), Toast.LENGTH_SHORT).show();
+//			}
+//		});;
 		return convertView;
 	}
 
