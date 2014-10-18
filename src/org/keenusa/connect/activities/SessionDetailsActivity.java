be0ed75715bfa151871a67992cbed993c0457ed0
comscience15 <comscience15@hotmail.com>
2014-10-18 09:52:48 -0700

    Fixed to cover null info on Session Details

--- src/org/keenusa/connect/activities/SessionDetailsActivity.java
+++ src/org/keenusa/connect/activities/SessionDetailsActivity.java
@@ -1,5 +1,9 @@
 package org.keenusa.connect.activities;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.joda.time.DateTime;
 import org.keenusa.connect.R;
 import org.keenusa.connect.models.KeenProgram;
 import org.keenusa.connect.models.KeenSession;
@@ -14,11 +18,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SessionDetailsActivity extends FragmentActivity {
-	private TextView tvProgramNameLabel, tvLocationLabel, tvDateLabel, tvProgramTypeLabel, tvProgramActiveDateLabel;
-	private TextView tvProgramName, tvLocation, tvDate, tvProgramType, tvProgramActiveDate;
+	private TextView tvProgramNameLabel, tvLocationLabel, tvDateLabel, tvProgramTypeLabel, tvProgramActiveDateLabel, tvAttCoachesLabel, tvAttAthletesLabel;
+	private TextView tvProgramName, tvLocation, tvDate, tvProgramType, tvProgramActiveDate, tvAttCoaches, tvAttAthletes;
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
 
-	// this is the session that is passed from session list activity or session list activity could pass session id if we use SQLite
 		KeenSession session;
 		KeenProgram program;
 
@@ -34,22 +37,35 @@ public class SessionDetailsActivity extends FragmentActivity {
 			session = (KeenSession) getIntent().getSerializableExtra("session");
 			program = (KeenProgram) getIntent().getSerializableExtra("program");
 			
-			tvDate.setText(session.getDate().toString().substring(0, 10));
+			tvDate.setText(formamtDate(session.getDate()));
 			tvProgramName.setText(program.getName());
-			String address = /*program.getLocation().getAddress1() + */program.getLocation().getAddress2() + " " +
-					 program.getLocation().getCity() + " " + program.getLocation().getState() + " " + program.getLocation().getZipCode();
-			tvLocation.setText(address);
-			tvProgramType.setText(program.getGeneralProgramType().toString());
-			tvProgramActiveDate.setText(session.getProgram().getProgramTimes());
-
+			if (program.getLocation() != null) {
+				String address = program.getLocation().getAddress2() + " " +
+						 program.getLocation().getCity() + " " + program.getLocation().getState() + " " + program.getLocation().getZipCode();
+				tvLocation.setText(address);
+			} else {
+				tvLocation.setText("No Address");
+			}
+			if (program.getGeneralProgramType() != null) {
+				tvProgramType.setText(program.getGeneralProgramType().toString());
+			} else {
+				tvProgramType.setText("No Program Type");
+			}
+			if (session.getProgram().getProgramTimes() != null) {
+				tvProgramActiveDate.setText(session.getProgram().getProgramTimes());
+			} else {
+				tvProgramActiveDate.setText("12pm - 1pm");
+			}
+			tvAttCoaches.setText(session.getRegisteredCoachCount()+"");
+			tvAttAthletes.setText(session.getRegisteredAthleteCount()+"");
+		
 		}
 
-//		@SuppressWarnings("deprecation")
-//		private CharSequence setDateTime(String dateTime) {
-//			Date date = new Date(dateTime);
-//			DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
-//			return dateFormat.format(date);
-//		}
+        private CharSequence formamtDate(DateTime date) {
+			DateTime dateTime = session.getDate();
+			String result = new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH).format(dateTime.toDate());
+			return result;
+		}
 
 		private void setView() {
 			tvProgramNameLabel = (TextView) findViewById(R.id.tvProgramNameLabel);
@@ -57,38 +73,18 @@ public class SessionDetailsActivity extends FragmentActivity {
 			tvDateLabel = (TextView) findViewById(R.id.tvDateLabel);
 			tvProgramActiveDateLabel = (TextView) findViewById(R.id.tvProgramActiveDateLabel);
 			tvProgramTypeLabel = (TextView) findViewById(R.id.tvProgramTypeLabel);
+			tvAttCoachesLabel = (TextView) findViewById(R.id.tvAttCoachesLabel);
+			tvAttAthletesLabel = (TextView) findViewById(R.id.tvAttAthletesLabel);
+			
 			tvProgramName = (TextView) findViewById(R.id.tvProgramName);
 			tvLocation = (TextView) findViewById(R.id.tvLocation);
 			tvDate = (TextView) findViewById(R.id.tvDate);
 			tvProgramActiveDate = (TextView) findViewById(R.id.tvProgramActiveDate);
 			tvProgramType = (TextView) findViewById(R.id.tvProgramType);
-
-			//		tvProgramName.setText(program.getName());
-			//		tvLocation.setLocation(program.getLocation());
-			//		tvDate.setText(program.getProgramTimes());
-			//		tvSessionPeriod.setText(program.getProgramPeriod);
+			tvAttCoaches = (TextView) findViewById(R.id.tvAttCoaches);
+			tvAttAthletes = (TextView) findViewById(R.id.tvAttAthletes);
 		}
 
-//	private void setupTabs() {
-//		ActionBar actionBar = getActionBar();
-//		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-//		actionBar.setDisplayShowTitleEnabled(true);
-//
-//		Tab coaches = actionBar.newTab().setText("Coaches")
-//				//			.setIcon(R.drawable.ic_home)
-//				.setTag("CoachesFragment")
-//				.setTabListener(new FragmentTabListener<CoachesFragment>(R.id.flContainer, this, "Coaches", CoachesFragment.class));
-//
-//		actionBar.addTab(coaches);
-//		actionBar.selectTab(coaches);
-//
-//		Tab athletes = actionBar.newTab().setText("Athletes")
-//				//			.setIcon(R.drawable.ic_mentions)
-//				.setTag("AtheletsFragment")
-//				.setTabListener(new FragmentTabListener<AtheletsFragment>(R.id.flContainer, this, "Athletes", AtheletsFragment.class));
-//
-//		actionBar.addTab(athletes);
-//	}
 		private void openCoachCheckIn(KeenSession session2, KeenProgram program2) {
 			Intent i = new Intent(this, CoachesCheckInActivity.class);
 			i.putExtra("session", session2);
@@ -112,9 +108,6 @@ public class SessionDetailsActivity extends FragmentActivity {
 		
 		@Override
 		public boolean onOptionsItemSelected(MenuItem item) {
-			// Handle action bar item clicks here. The action bar will
-			// automatically handle clicks on the Home/Up button, so long
-			// as you specify a parent activity in AndroidManifest.xml.
 			int id = item.getItemId();
 			if (id == R.id.miCoachesAttendance) {
 				openCoachCheckIn(session, program);
