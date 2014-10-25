642b89191e2a3f53e99161cf6df30524fe401086
anastasiya-ca <a.bashkirava@gmail.com>
2014-10-25 12:01:00 -0700

    login screen appearance changes

--- src/org/keenusa/connect/activities/SplahScreen.java
+++ src/org/keenusa/connect/activities/SplahScreen.java
@@ -1,25 +1,26 @@
 package org.keenusa.connect.activities;
 
 import org.keenusa.connect.R;
-import org.keenusa.connect.R.layout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 
 public class SplahScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		getActionBar().hide();
 		setContentView(R.layout.activity_splah_screen);
-		Thread timer = new Thread(){
-			public void run(){
-				try{
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
 					sleep(5000);
-				} catch (InterruptedException e){
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} finally {
 					Intent openMainIntent = new Intent(SplahScreen.this, LoginActivity.class);
@@ -35,6 +36,6 @@ public class SplahScreen extends Activity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		finish();    //terminate splash screen
+		finish(); //terminate splash screen
 	}
 }
