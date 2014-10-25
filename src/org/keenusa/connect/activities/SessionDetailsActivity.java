86496b26041652ea40a60f9699eea287ca783f78
comscience15 <comscience15@hotmail.com>
2014-10-24 21:35:38 -0700

    Get Background changes following Program name

--- src/org/keenusa/connect/activities/SessionDetailsActivity.java
+++ src/org/keenusa/connect/activities/SessionDetailsActivity.java
@@ -10,14 +10,29 @@ import org.keenusa.connect.models.KeenProgram;
 import org.keenusa.connect.models.KeenSession;
 import org.keenusa.connect.utilities.CheckinEditMode;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -28,27 +43,60 @@ public class SessionDetailsActivity extends FragmentActivity {
 	private TextView tvProgramName, tvLocation, tvDate, tvAttCoach, tvAttAthlete, tvProgramType, tvProgramTimes, tvAttCoaches, tvAttAthletes;
 	private Button btnCoachChkIn, btnAthleteChkIn;
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
+	private ImageView image;
 	ProgressBar _progressBarCoach, _progressBarAthlete;
 
 	KeenSession session;
 	KeenProgram program;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
 		setContentView(R.layout.activity_session_details);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setView();
 		setData();
+//		applyBlur();
 	}
+
+//    private void applyBlur() {
+//		Bitmap tempbg = BitmapFactory.decodeResource(getResources(), R.drawable.sports_two);
+//		Bitmap final_Bitmap = BlurImage(tempbg);
+//	}
+//
+//    @SuppressLint("NewApi")
+//	Bitmap BlurImage(Bitmap input) {
+//		try {
+//			RenderScript rs = RenderScript.create(getApplicationContext());
+//			Allocation alloc = Allocation.createFromBitmap(rs, input);
+//			
+//			ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+//			blur.setRadius(21);
+//			blur.setInput(alloc);
+//			
+//			Bitmap result = Bitmap.createBitmap(input.getWidth(), input.getHeight(), Bitmap.Config.ARGB_8888);
+//			Allocation outAlloc = Allocation.createFromBitmap(rs, result);
+//			
+//			blur.forEach(outAlloc);
+//			outAlloc.copyTo(result);
+//			rs.destroy();
+//			
+//			return result;
+//		}catch (Exception e) {
+//			return input;
+//		}
+//	}
 
 	private void setData() {
 		String address = "";
 		session = (KeenSession) getIntent().getSerializableExtra("session");
 		program = (KeenProgram) getIntent().getSerializableExtra("program");
-
+		
 		tvDate.setText(formamtDate(session.getDate()));
 		tvProgramName.setText(program.getName());
+		changeBackgroundImage(program.getName());
 		if (program.getLocation() != null) {
 			tvLocation.setText(program.getLocation().getLocationString());
 		} else {
@@ -74,6 +122,34 @@ public class SessionDetailsActivity extends FragmentActivity {
 
 		_progressBarAthlete.setProgress(session.getRegisteredAthleteCount());
 		tvAttAthlete.setText(session.getRegisteredAthleteCount() + "");
+		
+		
+	}
+
+	private void changeBackgroundImage(String name) {
+		if (name.equals("Sports 1")){
+			image.setImageResource(R.drawable.sport1);
+		} else if (name.equals("Sports 2")) {
+			image.setImageResource(R.drawable.sport2);
+		} else if (name.equals("Sports 1 & 2")) {
+			image.setImageResource(R.drawable.sport12);
+		} else if (name.equals("Basketball") || name.equals("Basketball Clinic - Hoops")) {
+			image.setImageResource(R.drawable.basketball);
+		} else if (name.equals("KEENquatics") || name.equals("KEENquatics - SWIM")) {
+			image.setImageResource(R.drawable.quatics);
+		} else if (name.equals("Summer Family Picnic & Games")) {
+			image.setImageResource(R.drawable.picnic);
+		} else if (name.equals("Kids Sports & Tennis - SFUHS")) {
+			image.setImageResource(R.drawable.kidssport);
+		} else if (name.equals("Kids and Young Adult Sports at YMCA") || name.equals("Kids Sports and Young Adult Sports - East Bay")) {
+			image.setImageResource(R.drawable.ymca);
+		} else if (name.equals("Basketball") || name.equals("Summer Family Pool Party or Basketball & Picnic")) {
+			image.setImageResource(R.drawable.basketball);
+		} else if (name.equals("Holiday Party 2012")) {
+			image.setImageResource(R.drawable.holidayparty);
+		} else if (name.equals("KEENGala: Pre-Event") || name.equals("KEENGala - Event Volunteer Shift 1") || name.equals("KEENGala - Event Volunteer Shift 2")) {
+			image.setImageResource(R.drawable.holidayparty);
+		} 
 	}
 
 	private CharSequence formamtDate(DateTime date) {
@@ -106,6 +182,7 @@ public class SessionDetailsActivity extends FragmentActivity {
 		_progressBarAthlete = (ProgressBar) findViewById(R.id.cicularProgressBarAthlete);
 		tvAttCoach = (TextView) findViewById(R.id.tvAttCoach);
 		tvAttAthlete = (TextView) findViewById(R.id.tvAttAthlete);
+		image = (ImageView) findViewById(R.id.ivSessionBackgroundPic);
 		//		tvAttCoaches = (TextView) findViewById(R.id.tvAttCoaches);
 		//		tvAttAthletes = (TextView) findViewById(R.id.tvAttAthletes);
 	}
